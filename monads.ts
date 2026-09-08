d4d9243697e6e015203f6fea9404604cdc32f1c3
Rohit Bhojak <rohitbhojak99@gmail.com>
2026-09-08 09:16:58 +0530

    Add simple logging

--- monads.ts
+++ monads.ts
@@ -1,9 +1,16 @@
-const square = (x: number) => {
-  return x * x;
+interface NumberWithLogs {
+  result: number;
+  logs: string[];
+}
+
+const square = (x: number): NumberWithLogs => {
+  const result = x * x;
+  return { result, logs: [`Sqaured ${x} to get ${result}`] };
 };
 
-const addOne = (x: number) => {
-  return x + 1;
+const addOne = (x: NumberWithLogs): NumberWithLogs => {
+  const result = x.result + 1;
+  return { result, logs: x.logs.concat([`Added 1 to get ${result}`]) };
 };
 
 console.log(addOne(square(3)));
