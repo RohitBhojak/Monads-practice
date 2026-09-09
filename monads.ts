bb59188e47a01956e39b29348cb64aa766043f99
Rohit <rohitbhojak99@gmail.com>
2026-09-09 09:52:27 +0000

    Implement monad design pattern

--- monads.ts
+++ monads.ts
@@ -7,15 +7,35 @@ const wrapNumberWithLogs = (x: number): NumberWithLogs => {
   return { result: x, logs: [] };
 };
 
-const square = (x: NumberWithLogs): NumberWithLogs => {
-  const result = x.result * x.result;
-  return { result, logs: x.logs.concat([`Sqaured ${x.result} to get ${result}`]) };
+const runWithLogs = (input: NumberWithLogs, transform: (_: number) => NumberWithLogs) => {
+  const newNumberWithLogs = transform(input.result);
+  return {
+    result: newNumberWithLogs.result,
+    logs: input.logs.concat(newNumberWithLogs.logs),
+  };
 };
 
-const addOne = (x: NumberWithLogs): NumberWithLogs => {
-  const result = x.result + 1;
-  return { result, logs: x.logs.concat([`Added 1 to get ${result}`]) };
+const square = (x: number): NumberWithLogs => {
+  const result = x * x;
+  return { result, logs: [`Squared ${x} to get ${result}`] };
 };
 
-console.log(addOne(square(wrapNumberWithLogs(3))));
-console.log(square(square(wrapNumberWithLogs(3))));
+const addOne = (x: number): NumberWithLogs => {
+  const result = x + 1;
+  return { result, logs: [`Added 1 to get ${result}`] };
+};
+
+const multiplyByThree = (x: number): NumberWithLogs => {
+  const result = x * 3;
+  return { result, logs: [`Multiplied ${x} by 3 to get ${result}`] };
+};
+
+const a = wrapNumberWithLogs(3);
+const b = runWithLogs(a, square);
+const c = runWithLogs(b, addOne);
+
+console.log(c);
+
+// New function can be plugged in easily
+const d = runWithLogs(c, multiplyByThree);
+console.log(d);
